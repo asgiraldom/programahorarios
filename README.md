9737bad7f3d6eff15c3adb8d0d101244d79fcae4
Andres Giraldo - Moto Partes <asgiraldom.gmail.com>
2023-11-30 15:10:53 -0600

    agregar gitignore y readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# steps
+##resumen este proyecto es para que automaticamente definiendo cuantas horas se quiere programar un empleado a la semana, este organice el horario de todos.##
+
--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 trabajadores = ["Andres Giraldo", "Juan Guillermo", "Yonatan Gomez", "Edwin Viera", "Kelly Bonilla"]
 
+#unidades de 30 minutos
+Horario_Lunes = 22
+Horaio_martes = 22
+
 print("andres giraldo")
