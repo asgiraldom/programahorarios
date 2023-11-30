7fc572cf889cdf4a74176010f93ab98019a0e10b
Andres Giraldo - Moto Partes <asgiraldom.gmail.com>
2023-11-30 15:27:00 -0600

    dias laborales

---
+++ programa.py
@@ -0,0 +1,6 @@
+trabajadores = ["Andres Giraldo", "Juan Guillermo", "Yonatan Gomez", "Edwin Viera", "Kelly Bonilla"]
+
+#unidades de 30 minutos
+DiasLaborales = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+print("andres giraldo")
